c02d06e0765df211ac63a2d51ef97aab4476d7af
Abdul Sami <sami.kdr@live.se>
2022-01-16 00:37:16 +0100

    questions.json backup and test ok

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv'); 
 
 dotenv.config(); 
-
-mongoose.connect(process.env.uri, {
+// Should change the uri to todo from .env if testing todo app
+mongoose.connect(process.env.uri_easy_korkort, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }); 
@@ -15,11 +15,34 @@ const todoSchema = new mongoose.Schema({
     item: String
 }); 
 
+const questionSchema = mongoose.Schema({
+    Question: String, 
+    Correct: String, 
+    Incorrect: [String], 
+    Image: String, 
+    Explanation: String,
+    Category: String, 
+}, {collection: 'Questions'}); 
+
+const Questions = mongoose.model('Questions', questionSchema); 
+
 const Todo = mongoose.model('Todo-list', todoSchema); 
 
 const urlencodedParser = bodyParser.urlencoded({extended: false}); 
 
 module.exports = app => {
+
+    app.get('/getallquestions', (req, res) => {
+       
+      //  res.send({message: 'req recieved...'}); 
+            
+            Questions.find({}, (error, data) => {
+               
+                if(error) throw error; 
+    
+                res.json(data); 
+            }); 
+    }); 
 
     app.get('/todo', (req, res) => {
 
